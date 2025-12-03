c0f9bb176113775b7cf113fe6fdd33387c741263
TimmyTheTaterTot <zackd365@gmail.com>
2025-12-02 18:23:31 -0800

    Updated about and leaderboard to be fully reactive

--- connect4app/src/about/about.jsx
+++ connect4app/src/about/about.jsx
@@ -2,7 +2,14 @@ import React from 'react';
 
 import "./about.css";
 
-export function About() {
+export function About(props) {
+  const [tip, setTip] = React.useState("Loading a super helpful tip...");
+
+  // Fetch data
+  React.useEffect(() => {
+    setTip("The pieces fall straight down, occupying the lowest available space within the column. The objective of the game is to be the first to form a horizontal, vertical, or diagonal line of four of one's own tokens.")
+  }, []);
+  
   return (
     <main className="text-center justify-content-center">
         <img className="picture mx-auto" src="https://upload.wikimedia.org/wikipedia/commons/8/84/Connect-four.jpg" />
@@ -10,9 +17,7 @@ export function About() {
             <p>
                 Connect 4 is a game in which the players choose a color and then take turns dropping colored tokens into a six-row, seven-column vertically suspended grid. 
             </p>
-            <p>
-                The pieces fall straight down, occupying the lowest available space within the column. The objective of the game is to be the first to form a horizontal, vertical, or diagonal line of four of one's own tokens.
-            </p>
+            <p> { tip } </p>
         </div>
     </main>
   );
--- connect4app/src/leaderboard/leaderboard.jsx
+++ connect4app/src/leaderboard/leaderboard.jsx
@@ -1,54 +1,55 @@
 import React from 'react';
 
 export function Leaderboard() {
-  return (
-    <main>
-        <table className="table table-dark table-striped table-hover">
-            <thead>
+    const [scores, setScores] = React.useState([]);
+
+    // Load data from DB
+    React.useEffect(() => {
+        const highScores = localStorage.getItem('highScores');
+        if (highScores) {
+            setScores(JSON.parse(highScores));
+        }
+    }, []);
+
+    const scoresList = [];
+    if (scores.length > 0) {
+        for (const [i, player] of scores.entries()) {
+            scoresList.push(
                 <tr>
-                    <th>Rank</th>
-                    <th>Name</th>
-                    <th>ELO</th>
-                    <th>Wins</th>
-                    <th>Losses</th>
-                    <th>W/L Ratio</th>
+                    <td>{i}</td>
+                    <td>{player.name}</td>
+                    <td>{player.elo}</td>
+                    <td>{player.wins}</td>
+                    <td>{player.losses}</td>
+                    <td>{(player.wins/player.gamesPlayed).toString().slice(0, 5)}</td>
                 </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>Zack</td>
-                    <td>1100</td>
-                    <td>6</td>
-                    <td>4</td>
-                    <td>0.600</td>
-                </tr>
-                <tr>
-                    <td>2</td>
-                    <td>Jake</td>
-                    <td>900</td>
-                    <td>8</td>
-                    <td>12</td>
-                    <td>0.400</td>
-                </tr>
-                <tr>
-                    <td>3</td>
-                    <td>Frank</td>
-                    <td>600</td>
-                    <td>2</td>
-                    <td>14</td>
-                    <td>0.125</td>
-                </tr>
-                <tr>
-                    <td>4</td>
-                    <td>Cleo</td>
-                    <td>200</td>
-                    <td>0</td>
-                    <td>16</td>
-                    <td>0.000</td>
-                </tr>
-            </tbody>
-        </table>
-    </main>
-  );
+            );
+        }
+    } else {
+        scoresList.push(
+            <tr key='0' className="text-center">
+                <td colSpan='6'>No rankings yet! Please come back after playing...</td>
+            </tr>
+        );
+    }
+
+    return (
+        <main>
+            <table className="table table-dark table-striped table-hover">
+                <thead>
+                    <tr>
+                        <th>Rank</th>
+                        <th>Name</th>
+                        <th>ELO</th>
+                        <th>Wins</th>
+                        <th>Losses</th>
+                        <th>W/L Ratio</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    { scoresList }
+                </tbody>
+            </table>
+        </main>
+    );
 }
