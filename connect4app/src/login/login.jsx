2bf0c3a5cd91f3b413003a2461259da93740e2c5
TimmyTheTaterTot <zackd365@gmail.com>
2026-04-09 23:54:23 -0700

    added todos to login.jsx

--- connect4app/src/login/login.jsx
+++ connect4app/src/login/login.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import { LoggedOut } from './logged_out';
 import { LoggedIn } from './logged_in';
 
+// TODO: visual cue when login fails
+// TODO: configure game results sending
 export function Login({ loginState, changeLoginState, username }) {
   return (
     <main className="container-fluid text-center">
