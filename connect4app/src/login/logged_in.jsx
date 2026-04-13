5554c6f29d05674cf33e58004d23dd0feec2f93c
TimmyTheTaterTot <zackd365@gmail.com>
2026-04-12 21:21:54 -0700

    [feat] add quote to login screen
    Used third-party service to obtain quote and display it on the login page if the user is logged in.

--- connect4app/src/login/logged_in.jsx
+++ connect4app/src/login/logged_in.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import './login.css'
+
 import { useNavigate } from 'react-router-dom';
 
 
@@ -7,6 +9,20 @@ export function LoggedIn({ username, onLogout }) {
     const navigate = useNavigate()
 
     const [errorMessage, setErrorMessage] = React.useState(null);
+    const [quoteBody, setQuoteBody] = React.useState('Loading...');
+    const [quoteAuthor, setQuoteAuthor] = React.useState('');
+
+    React.useEffect(() => {(async () => {
+        const res = await fetch('https://quote.cs260.click/', {
+            method: 'GET'
+        })
+        if (res.ok) {
+            const quote = await res.json();
+            console.log(quote);
+            setQuoteBody(quote.quote);
+            setQuoteAuthor(quote.author);
+        }
+    })()}, []);
 
     async function logout() {
         localStorage.removeItem('username');
@@ -22,14 +38,18 @@ export function LoggedIn({ username, onLogout }) {
     }
 
     return (
-    <div>
-        <h2 className="text-light text-nowrap">Welcome to Connect 4 Club</h2>
-        <h1 className="text-light text-nowrap">{ username }</h1>
-        {errorMessage && <div className='mb-2'>
-            <span style={{ color: '#FF5A5F' }} className='fw-bold fs-5'>{errorMessage}</span>
-        </div>}
-        <button className="btn btn-primary mx-auto px-4" onClick={ () => navigate('/play') }>Play</button>
-        <button className="btn bg-mid text-white mx-auto px-4" onClick={ () => logout() }>Log Out</button>
-    </div>
+        <div>
+            <h2 className="text-light text-nowrap">Welcome to Connect 4 Club</h2>
+            <h1 className="text-light text-nowrap">{ username }</h1>
+            {errorMessage && <div className='mb-2'>
+                <span style={{ color: '#FF5A5F' }} className='fw-bold fs-5'>{errorMessage}</span>
+            </div>}
+            <button className="btn btn-primary mx-auto px-4" onClick={ () => navigate('/play') }>Play</button>
+            <button className="btn bg-mid text-white mx-auto px-4" onClick={ () => logout() }>Log Out</button>
+            <div className="mt-5 mb-2 text-white quote-text">{quoteBody}</div>
+            {quoteAuthor && 
+                <span className="fw-bold text-white">- {quoteAuthor}</span>
+            }
+        </div>
     );
 }
--- connect4app/src/login/logged_out.jsx
+++ connect4app/src/login/logged_out.jsx
@@ -42,21 +42,21 @@ export function LoggedOut({ onLogin }) {
     }
 
     return (
-    <div>
-        <h1 className="text-light text-nowrap">Connect 4 Club Login</h1>
-        <div className="input-group mb-3">
-            <span className="input-group-text">Username:</span>
-            <input className="form-control" onChange={(e) => setUsername(e.target.value)} type="text" placeholder="example@email.com" />
+        <div>
+            <h1 className="text-light text-nowrap">Connect 4 Club Login</h1>
+            <div className="input-group mb-3">
+                <span className="input-group-text">Username:</span>
+                <input className="form-control" onChange={(e) => setUsername(e.target.value)} type="text" placeholder="example@email.com" />
+            </div>
+            <div className="input-group mb-3">
+                <span className="input-group-text">Password:</span>
+                <input className="form-control" onChange={(e) => setPassword(e.target.value)} type="password" placeholder="password" />
+            </div>
+            {errorMessage && <div className='mb-2'>
+                <span style={{ color: '#FF5A5F' }} className='fw-bold fs-5'>{errorMessage}</span>
+            </div>}
+            <button className="btn btn-primary mx-auto px-4" onClick={ () => login() } disabled={ buttonsDisabled }>Login</button>
+            <button className="btn bg-mid text-white mx-auto px-4" onClick={ () => createUser() } disabled={ buttonsDisabled }>Register</button>
         </div>
-        <div className="input-group mb-3">
-            <span className="input-group-text">Password:</span>
-            <input className="form-control" onChange={(e) => setPassword(e.target.value)} type="password" placeholder="password" />
-        </div>
-        {errorMessage && <div className='mb-2'>
-            <span style={{ color: '#FF5A5F' }} className='fw-bold fs-5'>{errorMessage}</span>
-        </div>}
-        <button className="btn btn-primary mx-auto px-4" onClick={ () => login() } disabled={ buttonsDisabled }>Login</button>
-        <button className="btn bg-mid text-white mx-auto px-4" onClick={ () => createUser() } disabled={ buttonsDisabled }>Register</button>
-    </div>
     );
 }
