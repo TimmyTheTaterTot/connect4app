af7fdd795fc943714fe1ae48a94ea64077e16129
TimmyTheTaterTot <zackd365@gmail.com>
2026-06-06 21:28:51 -0600

    [ci/cd] Add connection timeout to update script

--- connect4app/deployToDocker.sh
+++ connect4app/deployToDocker.sh
@@ -33,7 +33,7 @@ podman build -f dockerfile -t git.darlingdigital.net/zack/connect4app:$TAG .
 podman push git.darlingdigital.net/zack/connect4app:$TAG
 
 printf "\n----> Triggering container rebuild to fetch new image\n"
-ssh zack@192.168.1.81 << ENDSSH
+ssh -o ConnectTimeout=2 zack@192.168.1.81 << ENDSSH
 cd /srv/connect4app/
 podman compose up --pull always -d
 ENDSSH
